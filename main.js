7d32a5c1e68f909424241f31f898e5a3e9e86e53
Karan Nepali <ikrasnarak786@gmail.com>
2024-02-28 20:07:28 +0545

    change

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ require('dotenv').config()
  
  const express = require("express")
  const app = express()
- const port = 4000;
+ const port = 3000;
+
  app.get("/",(req, res)=>{
     res.send("Hello world")
+ })
+ app.get("/twitter",(req,res)=>{
+   res.send("Twitter is responding")
  })
  app.get("/youtube",(req,res)=>{
     res.send("Please subscribe to my youtube channel")
